4a81141e3ab31b9ff65b0e113ee08db56e5d7665
OlmezSercam <154369106+OlmezSercam@users.noreply.github.com>
2023-12-20 13:11:02 +0300

    Member methodları
    Methodlar hakkında bir çalışma

---
+++ MembersandFinalizers/MembersandFinalizers/Class1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MembersandFinalizers
+{
+    internal class Members
+    {
+        //private field
+        private string memberName;
+        private string jobTitle;
+        private int salary ;
+
+        //public field
+        public int age;
+
+        //property
+        public string JobTitle
+        {
+            get
+            {
+                return JobTitle;
+            }
+            set
+            {
+                jobTitle = value;
+            } 
+        }
+
+        //public method
+        public void Introducing(bool isFriend)
+        {
+            if (isFriend)
+            {
+                SharingPrivateInfo();
+            }
+            else
+            {
+                Console.WriteLine("Hi, my name is {0}, and my job title is {1}." +
+                    " I'm {2} years old.", memberName, jobTitle, age);
+            }
+        }
+        //private method
+        private void SharingPrivateInfo()
+        {
+            
+            Console.WriteLine("My salary is " + salary);
+        }
+        // constructor
+        public Members()
+        {
+            age = 30;
+            memberName = "Lucy";
+            salary = 60000;
+            jobTitle = "Developer";
+            Console.WriteLine("Object created");
+        }
+        // Finalize - Destructor
+        ~Members()
+        {
+            //cleanup statements
+            Console.WriteLine("Deconstruction of Members Object");
+            Debug.Write("Destruction of Members object");
+        }
+    }
+}
