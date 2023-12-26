45c813d73c067b3d12df29aa7a9bfec470eb542b
OlmezSercam <154369106+OlmezSercam@users.noreply.github.com>
2023-12-26 14:22:08 +0300

    Queue
    Queue stud

---
+++ Queues and Stacks/Queues and Stacks/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+namespace Stacks
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Stackte Veri en üste eklenir veya en üstten çıkarılır aşağıya erişim yoktur.
+            //undo butonu veya tarayıcılardaki geri gitme butonları stack
+            //Queue veri en arkadan girer ve en önden çıkar sırada beklemek gibi
+
+            //defining a new stack 
+            Stack<int> stack = new Stack<int>();
+
+            // add elements to the stack using the Push()
+            stack.Push(1);
+            stack.Push(137);
+
+            //Peek() will return the element at the top of the stack without removing it
+            Console.WriteLine("Top value in the stack is: {0}", stack.Peek());
+
+
+            //removing the element Pop()
+            stack.Pop();
+
+            // silinecek elementin stacklenmesi
+            int myStackItem = stack.Pop();
+
+
+            while (stack.Count > 0)
+            {
+                Console.WriteLine("The Top Value {0} was removed from the stack", stack.Pop);
+                Console.WriteLine("Current stack count is {0}", stack.Count);
+            }
+            
+            // array içindeki sayıları stack içerisine yazma
+            int[] numbers = new int[] { 8, 2, 3, 4, 7, 6, 2 }; //array
+            // defining a new stack
+            Stack<int> myStack = new Stack<int>();
+            Console.WriteLine("the numbers in the array are: ");
+            foreach (int number in numbers)
+            {
+                // print it
+                Console.WriteLine(number + " ");
+                //push it into our stack(add)
+                myStack.Push(number);
+            }
+
+            // array içindeki sayıları stack içerisinden çıkarırken tersten yazmak
+            Console.WriteLine(" ");
+            Console.WriteLine("the numbers in reverse: ");
+
+            while (myStack.Count>0)
+            {
+                int number = myStack.Pop();
+                Console.Write(number + " ");
+            }  
+        }
+    }
+}
---
+++ Queues/Queues/Program.cs
@@ -0,0 +1,103 @@
+﻿namespace Queues
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+              // defining a queue of integers
+            Queue<int> queue = new Queue<int>();
+
+              //sıranın sonuna element ekler ----> Enqueue()
+            queue.Enqueue(1);
+
+              //printing the element at the front of the queue ---> Peek()
+            Console.WriteLine("The value at the front of the queue is {0}", queue.Peek());
+
+ 
+              //Arkaya eklemek öndekini değiştirmez Stack gibi değil
+            queue.Enqueue(2);
+            Console.WriteLine("The value at the front of the queue is {0}", queue.Peek());
+            queue.Enqueue(3);
+            Console.WriteLine("The value at the front of the queue is {0}", queue.Peek());
+
+              //Sıranın önündekini çıkartmak ----> Dequeue()
+            queue.Dequeue();
+            Console.WriteLine("The value at the front of the queue is {0}", queue.Peek());
+              //Sıranın önündekini storelamak
+            //int Siraonu = queue.Dequeue();
+
+            while (queue.Count > 0)
+            {
+                //Dequeue() will return the element that was removed from the queue
+                Console.WriteLine("The front value {0} was removed from the queue",queue.Dequeue());
+
+                //Print the queue count
+                Console.WriteLine("Current queue count is : {0}", queue.Count);
+            }*/
+            Queue<Order> ordersQueue = new Queue<Orders>();
+
+            foreach (Order O in RecieveOrdersFromBranch1()) 
+            {
+                //add each order to the queue
+                ordersQueue.Enqueue(O);
+            }
+            foreach (Order O in RecieveOrdersFromBranch2())
+            {
+                ordersQueue.Enqueue(O);
+            }
+            while (ordersQueue.Count > 0);
+            {
+                //remove the order at the front of the queue
+                //and store it in a variable called currentOrder
+                Order currentOrder = ordersQueue.Dequeue();
+                //process the order
+                currentOrder.ProcessOrder();
+            }
+        }
+        // this method will create an array of orders and return it
+        static Order[] RecieveOrdersFromBranch1()
+        {
+            //creating new orders array
+            Order[] orders = new Order[]
+            {
+                new Order(1,5),
+                new Order(2,4),
+                new Order(6,10)
+            };
+            return orders;
+        }
+        static Order[] RecieveOrdersFromBranch2()
+        {
+            //creating new orders array
+            Order[] orders = new Order[]
+            {
+                new Order(3,5),
+                new Order(4,4),
+                new Order(5,10)
+            };
+            return orders;
+        }
+    }
+    class Order
+    {
+        // order ID
+        public int OrderID { get; set; }
+
+        // quantity of order
+        public int OrderQuantity { get; set; }
+
+        //simple Constructor
+        public Order(int id, int orderQuantity)
+        {
+            this.OrderId = id;
+            this.OrderQuantity = orderQuantity;
+        }
+
+        //print message on the screen that the order was processed
+        public void ProcessOrder()
+        {
+            Console.WriteLine($"Order {OrderId} processed!.");
+        }
+    }
+}   
