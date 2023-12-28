b0f2954950bf94179f76dc0d41f9731ab8839f25
OlmezSercam <154369106+OlmezSercam@users.noreply.github.com>
2023-12-28 13:04:18 +0300

    IEnumerable
    IEnumerable

---
+++ IEnumerable Example2/IEnumerable Example2/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace IEnumerable_Example2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // IEnumerable'ı method olarak kullanmak
+            List<int> numberList = new List<int>() { 8, 6, 2 };
+            int[] numberArray = new int[] { 1, 7, 1, 3 };
+            Console.WriteLine(" ");
+            CollectionSum(numberList);
+            Console.WriteLine(" ");
+            CollectionSum(numberArray);
+        }
+
+        static void CollectionSum(IEnumerable<int> anyCollection)
+        {
+            int sum = 0;
+            foreach (int num in anyCollection)
+            {
+                sum += num;
+            }
+            Console.Write("Sum is {0}",sum);
+        }
+    }
+}
