834eae11eb29f9a27c5d07d1119334c365434a94
OlmezSercam <154369106+OlmezSercam@users.noreply.github.com>
2023-12-28 13:06:21 +0300

    Inheritance2

---
+++ Inheritance2/Inheritance2/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Inheritance2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dog Dog = new Dog("Mahir", 23);
+            Console.WriteLine($"{Dog.Name} is {Dog.Age} years old");
+            Dog.MakeSound();
+            Dog.Play();
+            Dog.Eat();
+        }
+    }
+}
