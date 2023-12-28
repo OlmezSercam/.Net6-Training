b2af6e7975faa88437108ac37239c443e7e8bfb2
OlmezSercam <154369106+OlmezSercam@users.noreply.github.com>
2023-12-28 13:07:15 +0300

    Interface

---
+++ Interface/Interface/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Interface
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Ticket t1 = new Ticket(10);
+            Ticket t2 = new Ticket(10);
+            Console.WriteLine(t2.Equals(t1));
+        }
+    }
+}
---
+++ Interface/Interface/Ticket.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interface
+{
+    internal class Ticket : IEquatable<Ticket>
+    {
+        //properties
+        public int DurationInHours { get; set; }
+
+        //Constructor
+        public Ticket(int durationInHours)
+        {
+            this.DurationInHours = durationInHours;
+        }
+
+        public bool Equals(Ticket other)
+        {
+            return this.DurationInHours == other.DurationInHours;
+        }
+    }
+}
