a711701f66e3116762939d525bda99fe31769312
OlmezSercam <154369106+OlmezSercam@users.noreply.github.com>
2023-12-29 18:21:06 +0300

    Abstracts

---
+++ AbstractClassesC/AbstractClassesC/Shape.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractClassesC
+{
+    //Abstract classların içerisine üye oluşturulamaz fakat
+    // içinde bulunan methodlar ve propertyler kullanılabilir.
+    abstract class Shape
+    {
+        public string Name { get; set; }
+        public  virtual void GetInfo()
+        {
+            Console.WriteLine($"\nThis is a {Name}");
+        }
+        public abstract double Volume();
+    }
+}
