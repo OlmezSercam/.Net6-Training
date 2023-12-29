4319e324e3b08e181b78af3807b3d5e800dc5adb
OlmezSercam <154369106+OlmezSercam@users.noreply.github.com>
2023-12-29 18:28:32 +0300

    Read From A Text File

---
+++ Read From A Text File/Read From A Text File/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Read_From_A_Text_File
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Example 1 - Reading text
+            string text = System.IO.File.ReadAllText(@"C:\C# TextFile\Read.txt");
+            Console.WriteLine("TextFile contains following text: {0}",text);
+
+            //Example 2 - Reading Text
+            string[] Lines = System.IO.File.ReadAllLines(@"C:\C# TextFile\Read.txt");
+            Console.WriteLine("Contents of TextFile = ");
+            foreach (string line in Lines)
+            {
+                Console.WriteLine("\t" + line);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
