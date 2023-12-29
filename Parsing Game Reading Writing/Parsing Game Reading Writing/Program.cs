7384f120899c2129ec8ecfb286f113e7404af379
OlmezSercam <154369106+OlmezSercam@users.noreply.github.com>
2023-12-29 18:27:15 +0300

    Parsing Game

---
+++ Parsing Game Reading Writing/Parsing Game Reading Writing/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Parsing_Game_Reading_Writing
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] lines = System.IO.File.ReadAllLines(@"C:\C# TextFile\input.txt");
+            
+
+            foreach (string line in lines)
+            {
+
+                if (line.Contains("split"))
+                {
+                    string[] lines = line.Split(' ');
+                    Console.WriteLine(line.Split(" "));
+                }
+            }
+            Console.Read();
+        }
+    }
+}
