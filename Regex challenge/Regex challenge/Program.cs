9ebceb5093de32e3d7cbdf1131b2ddd9bdf874b2
OlmezSercam <154369106+OlmezSercam@users.noreply.github.com>
2023-12-29 18:28:52 +0300

    RegEx Challenge

---
+++ Regex challenge/Regex challenge/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Regex_challenge
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string pattern = @"\d{2,3}";
+            Regex regex = new Regex(pattern);
+            string text = System.IO.File.ReadAllText(@"C:\C# TextFile\input2.txt");
+            MatchCollection matches = regex.Matches(text);
+
+            foreach (Match match in matches)
+            {
+                int integerValue;
+                if (int.TryParse(match.Value, out integerValue))
+                {
+                    Console.WriteLine((char)integerValue+" ");
+                }
+            }
+            Console.ReadLine();
+            
+        }
+    }
+}
