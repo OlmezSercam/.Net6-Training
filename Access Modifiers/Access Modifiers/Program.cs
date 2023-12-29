38f9b8badbb111a7711d4d9fb3eb91810e49c1d1
OlmezSercam <154369106+OlmezSercam@users.noreply.github.com>
2023-12-29 18:21:27 +0300

    Access Modifiers

---
+++ Access Modifiers/Access Modifiers/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Access_Modifiers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
